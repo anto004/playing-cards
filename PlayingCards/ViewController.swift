d8b6f8ae498e628fd3f9e1828becdcdc030d0360
anto004 <anto_004@live.com>
2018-08-17 22:21:24 -0700

    added multiple cards on a screen to move them around

--- PlayingCards/ViewController.swift
+++ PlayingCards/ViewController.swift
@@ -4,33 +4,7 @@ class ViewController: UIViewController {
     
     var deck = PlayingCardDeck();
     
-    @IBOutlet weak var playingCardView: PlayingCardView! {
-        didSet {
-            let swipe = UISwipeGestureRecognizer(target: self, action: #selector(nextCard))
-            swipe.direction = [.left, .right];
-            playingCardView.addGestureRecognizer(swipe)
-            
-            let pinch = UIPinchGestureRecognizer(target: playingCardView, action: #selector(playingCardView.adjustFaceCardScale(byHandlingGestureRecognizedBy:)))
-            playingCardView.addGestureRecognizer(pinch)
-        }
-    }
-    
-    @objc
-    func nextCard() {
-        if let card = deck.draw() {
-            playingCardView.rank = card.rank.order;
-            playingCardView.suit = card.suit.rawValue;
-        }
-    }
-    
-    @IBAction func flipCard(_ sender: UITapGestureRecognizer) {
-        switch sender.state {
-        case .ended:
-            playingCardView.isFaceUp = !playingCardView.isFaceUp;
-        default:
-            break;
-        }
-    }
+    @IBOutlet var viewCards: [PlayingCardView]!
     
     override func viewDidLoad() {
         super.viewDidLoad();
