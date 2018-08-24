4d58ef49c19f90dd9382b2efae26bc08530f93fa
anto004 <anto_004@live.com>
2018-08-24 16:47:40 -0700

    flip all cards and when two cards are flipped up they will automatically flip back down

--- PlayingCards/ViewController.swift
+++ PlayingCards/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         }
         
         for cardView in cardViews {
-            cardView.isFaceUp = true;
+            cardView.isFaceUp = false;
             let card = cards.remove(at: cards.count.arc4Random);
             cardView.rank = card.rank.order;
             cardView.suit = card.suit.rawValue;
@@ -26,12 +26,36 @@ class ViewController: UIViewController {
         
     }
     
+    private var faceUpCardViews: [PlayingCardView] {
+        return cardViews.filter({$0.isFaceUp && !$0.isHidden})
+    }
+    
     @objc
     func flipCard(_ recognizer: UITapGestureRecognizer){
         switch recognizer.state {
         case .ended:
             if let chosenCardView = recognizer.view as? PlayingCardView {
-                chosenCardView.isFaceUp = !chosenCardView.isFaceUp;
+                UIView.transition(with: chosenCardView,
+                                  duration: 0.6,
+                                  options: [.transitionFlipFromLeft],
+                                  animations: {
+                                    chosenCardView.isFaceUp = !chosenCardView.isFaceUp;
+                                  },
+                                  completion: { finished in
+                                        if self.faceUpCardViews.count == 2 {
+                                            self.faceUpCardViews.forEach({cardView in
+                                                UIView.transition(with: cardView,
+                                                                  duration: 0.6,
+                                                                  options: [.transitionFlipFromLeft],
+                                                                  animations: {
+                                                                    cardView.isFaceUp = false;
+                                                                  },
+                                                                  completion: {finished in
+                                                                    
+                                                })
+                                            })
+                                        }
+                                    })
             }
         default:
             break;
