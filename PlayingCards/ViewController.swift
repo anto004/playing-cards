f3cb3b3c616063ec5e8dfa56569fc60da781aef0
anto004 <anto_004@live.com>
2018-08-24 16:31:30 -0700

    add tap gesture on each card

--- PlayingCards/ViewController.swift
+++ PlayingCards/ViewController.swift
@@ -3,16 +3,38 @@ import UIKit
 class ViewController: UIViewController {
     
     var deck = PlayingCardDeck();
+
     
-    @IBOutlet var viewCards: [PlayingCardView]!
-    
+    @IBOutlet var cardViews: [PlayingCardView]!
     override func viewDidLoad() {
         super.viewDidLoad();
         
-        for _ in 1...10 {
+        var cards = Array<PlayingCard>();
+        for _ in 1...(cardViews.count + 1) / 2 {
             if let card = deck.draw() {
-                print("\(card)");
+                cards += [card, card];
             }
+        }
+        
+        for cardView in cardViews {
+            cardView.isFaceUp = true;
+            let card = cards.remove(at: cards.count.arc4Random);
+            cardView.rank = card.rank.order;
+            cardView.suit = card.suit.rawValue;
+            cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(flipCard(_:))))
+        }
+        
+    }
+    
+    @objc
+    func flipCard(_ recognizer: UITapGestureRecognizer){
+        switch recognizer.state {
+        case .ended:
+            if let chosenCardView = recognizer.view as? PlayingCardView {
+                chosenCardView.isFaceUp = !chosenCardView.isFaceUp;
+            }
+        default:
+            break;
         }
     }
 }
